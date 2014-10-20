249ac43678f0b23ccaea81d9125196a7a0694848
ZacMillionaire <dev@jotunga.com>
2014-10-20 11:34:07 +1000

    adding series hiding and highlighting
    totally not pointless

--- data/monthlyGraph.php
+++ data/monthlyGraph.php
@@ -61,12 +61,67 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		    
 
 		var keyList = ["totalWeight","totalReps","avgReps","avgWeight","participants","uniqueExerciseCount"];
+		var textList = ["Total Weight","Total Reps","Average Weight","Average Reps","Players Tested","Exercises Used"];
 		var colorList = ["#00ff40","#ff0008","#ffb700","#0fd","#ff00c3","#07f","#80ff00"];
 
 		var div = d3.select("body").append("div")   
 			.attr("class", "tooltip")
 			.style("opacity", 0);
 		var dateString = d3.time.format("%d/%m/%Y");
+
+		var legend = svg.selectAll(".legend")
+			.data(keyList.slice())
+			.enter()
+				.append("g")
+				.attr("class", "legend")
+				.attr("transform", function (d, i) { 
+					return "translate(0," + i * 20 + ")"; 
+				})
+				.on("click", function(d){
+
+					// Determine if current line is visible
+					if(d3.selectAll("#"+d).style("display") == "inline") {
+						d3.selectAll("#"+d).style("display","none");
+						return;
+					}
+					d3.selectAll("#"+d).style("display","inline");
+					
+				})
+				.on("mouseover",function(d){
+					d3.select("#"+d)
+						.style("stroke-width",5);
+
+					d3.selectAll("circle#"+d)
+						.transition()
+						.attr("r",10);
+				})
+				.on("mouseout",function(d){
+					d3.selectAll("#"+d)
+						.transition()
+						.style("stroke-width",1.5);
+					d3.selectAll("circle#"+d)
+						.transition()
+						.attr("r",5);
+				});
+
+        legend.append("rect")
+            .attr("x", width - 10)
+            .attr("width", 10)
+            .attr("height", 10)
+            .style("fill", function (d,i) {
+            	return colorList[i];
+            })
+            .style("stroke", "grey");
+
+        legend.append("text")
+            .attr("x", width - 20)
+            .attr("y", 6)
+            .attr("dy", ".35em")
+            .style("text-anchor", "end")
+            .style("color", function (d,i) {
+            	return colorList[i];
+            })
+            .text(function (d,i) { return textList[i]; });
 
 		for(i = 0; i < keyList.length; i++){
 
@@ -81,7 +136,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				.datum(data)
 				.attr("class", "line")
 				.style("stroke", colorList[i])
-				.attr("d", line);
+				.attr("d", line)
+				.attr("id",keyList[i]);
 			svg.selectAll(".dot")
 				.data(data)
 				.enter().append("circle")
@@ -89,18 +145,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				.attr('cy', function(d) { return y(d[keyList[i]]); })
 				.attr('r', 5)
 				.attr('fill', colorList[i])
+				.attr("id",keyList[i])
 				.on('mouseover', function(d,i) {
 					div.transition()        
 		                .duration(200)      
 		                .style("opacity", 1);      
 		            div.html(
 		            	"<div class=\"tooltip-header\">Breakdown for: "+dateString(d.dateEntered)+"</div>"+
-		            	"<div class=\"tooltip-row\">Total Weight: "+d.totalWeight+"kg</div>"+
-		            	"<div class=\"tooltip-row\">Total Reps: "+d.totalReps+"</div>"+
-		            	"<div class=\"tooltip-row\">Average Weight: "+d.avgWeight+"kg</div>"+
-		            	"<div class=\"tooltip-row\">Average Reps: "+d.avgReps+"</div>"+
-		            	"<div class=\"tooltip-row\">Players Tested: "+d.participants+"</div>"+
-		            	"<div class=\"tooltip-row\">Exercises Used: "+d.uniqueExerciseCount+"</div>"
+		            	"<div class=\"tooltip-row\">"+textList[0]+": "+d.totalWeight+"kg</div>"+
+		            	"<div class=\"tooltip-row\">"+textList[1]+": "+d.totalReps+"</div>"+
+		            	"<div class=\"tooltip-row\">"+textList[2]+": "+d.avgWeight+"kg</div>"+
+		            	"<div class=\"tooltip-row\">"+textList[3]+": "+d.avgReps+"</div>"+
+		            	"<div class=\"tooltip-row\">"+textList[4]+": "+d.participants+"</div>"+
+		            	"<div class=\"tooltip-row\">"+textList[5]+": "+d.uniqueExerciseCount+"</div>"
 		            )  
 	                .style("left", (d3.event.pageX + 10) + "px")     
 	                .style("top", (d3.event.pageY) + "px");    
